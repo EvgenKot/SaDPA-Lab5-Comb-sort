eca36b45cdea4197292a892d4a8118b54cb73364
Evgeny Kot <ukal.evgen@gmail.com>
2022-04-04 01:10:46 +0500

    No

--- main.c
+++ main.c
@@ -10,3 +10,34 @@ Lab5
 В одномерном массиве упорядочить нечетные элементы, 
 оставив четные на местах.
 */
+
+void PrintArray(int *arr, int n){
+    int i;
+    for (i = 0; i<n; i++){
+        printf("%d ", arr[i]);
+    }
+}
+
+
+int main() {
+    int *arr;
+    int n;
+    int i;
+
+    printf("Len of arr: ");
+    scanf("%d", &n);
+    
+    arr = (int*)malloc(n * sizeof(int));
+    for (i = 0; i<n; i++){
+        printf("a[%d] = ", i);
+        scanf("%d", &arr[i]);
+    }
+
+    PrintArray (arr, n);
+    
+    
+
+    
+    free(arr);
+    return 0;
+}
