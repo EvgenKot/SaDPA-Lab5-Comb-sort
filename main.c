dbb837725b2f6fe164153f0d95a78edbf7d72212
Evgeny Kot <ukal.evgen@gmail.com>
2022-04-04 18:57:43 +0500

    v0.9
    Работает

--- main.c
+++ main.c
@@ -6,27 +6,89 @@
 Ращупкин Евгений КЭ-203
 28 вариант
 Lab5
-Сортировка Прямым выбором
+Сортировка Расчёской
 В одномерном массиве упорядочить нечетные элементы, 
 оставив четные на местах.
 */
+
 
 void PrintArray(int *arr, int n){
     int i;
     for (i = 0; i<n; i++){
         printf("%d ", arr[i]);
     }
+    printf("\n");
+}
+
+
+
+
+void CombSort(int *arr, int size){
+    int elementRight, buff;
+    for (int i = 0; i < size; i++) {
+        // Сравниваем попарно.
+        // Начинаем с первого и последнего элемента, затем постепенно уменьшаем
+        // диапазон сравниваемых значеный.
+        for (int j = 0; j < i + 1; j++) {
+
+            // Индекс правого элемента в текущей итерации сравнения
+            elementRight = (size - 1) - (i - j);
+
+            if (arr[j] > arr[elementRight]) {
+
+                buff              = arr[j];
+                arr[j]            = arr[elementRight];
+                arr[elementRight] = buff;
+            }
+
+        }
+    }
+}
+
+
+
+int NuberOfOdd(int *arr, int size){
+    int j = 0;
+    for (int i = 0; i < size; i++){
+        if (arr[i] % 2 != 0){
+            j++;
+        }
+    }
+    return j;
+}
+
+
+void OddFromArray(int *arr, int size, int *arr2){
+    int j = 0;
+    for (int i = 0; i < size; i++){
+        if (arr[i] % 2 != 0){
+            arr2[j] = arr[i];    
+            j++;      
+        }
+    }
+}
+
+void ReplacedOdd(int *arr, int size, int *arr2){
+    int j = 0;
+    for (int i = 0; i < size; i++){
+        if (arr[i] % 2 != 0){
+            arr[i] = arr2[j];
+            j++;      
+        }
+    }
 }
 
 
 int main() {
     int *arr;
+    int *arr2;
     int n;
+    int n2;
     int i;
 
     printf("Len of arr: ");
     scanf("%d", &n);
-    
+
     arr = (int*)malloc(n * sizeof(int));
     for (i = 0; i<n; i++){
         printf("a[%d] = ", i);
@@ -34,10 +96,23 @@ int main() {
     }
 
     PrintArray (arr, n);
-    
-    
 
+    n2 = NuberOfOdd(arr, n);
+    arr2 = (int*)malloc(n2 * sizeof(int));
+    printf("arr2 len = %d\n", n2);
+    printf("Odd:\n");
+    OddFromArray(arr, n, arr2);
+    PrintArray (arr2, n2);
+
+    CombSort(arr2, n2);
+    printf("Sorted Odd:\n");
+    PrintArray (arr2, n2);
+
+    ReplacedOdd (arr, n, arr2);
+    printf("Replacement:\n");
+    PrintArray (arr, n);
     
     free(arr);
+    free(arr2);
     return 0;
 }
